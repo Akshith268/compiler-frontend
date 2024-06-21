7a6d1649b8f2ca381fe7f705a37f911d27db5405
Akshith268 <akshith268@gmail.com>
2024-06-21 22:16:19 +0530

    second

--- src/pages/code.js
+++ src/pages/code.js
@@ -11,14 +11,21 @@ export default function Code() {
   const [jobId, setJobId] = useState("");
   const [status, setStatus] = useState("");
   const [error, setError] = useState(""); 
+  const [statusColor, setStatusColor] = useState("red");
+  const [loadingImg, setLoadingImg] = useState(null);
+
+  const loadingURL = 'https://media4.giphy.com/media/sSgvbe1m3n93G/200w.webp?cid=790b7611z0ipof4amwfgve7p7w2705wrgj9rq26lldnttf2x&ep=v1_gifs_search&rid=200w.webp&ct=g';
+  const successURL = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQWN-SLzk5eeEuA9zBJKzsM0qbvtLsKDfJ-w&s';
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     setError("");
     setStatus("PENDING");
     setOutput("");
+    setStatusColor("red");
+    setLoadingImg(loadingURL);
 
-    try{
+    try {
       const response = await axios.post('https://compilerbackend-qkvcy69k.b4a.run/compile', {
         language: language,
         code: code,
@@ -37,6 +44,8 @@ export default function Code() {
             const { status: jobStatus, output: jobOutput } = job;
             setStatus(jobStatus);
             if (jobStatus !== "PENDING") {
+              setStatusColor("green");
+              setLoadingImg(successURL);
               setOutput(jobOutput);
               clearInterval(intervalId);
             }
@@ -64,52 +73,49 @@ export default function Code() {
   }
 
   return (
-    <div>
+    <div className='code'>
       <div className="code-container">
-      <div className="code-editor">
-        <h1>Write your Code</h1>
-        <form onSubmit={handleSubmit}>
-          <div className="editor-label">Language</div>
-          <select name="language" value={language} onChange={(e) => setLanguage(e.target.value)}>
-            <option value="cpp">C++</option>
-            <option value="python">Python</option>
-          </select>
-
-          <div className="editor-label">Code</div>
-          <Editor
-            className='Editor'
-            height="80vh"
-            width="120vh"
-            defaultPath="code.cpp"
-            language={language}
-            value={code}
-            onChange={(value) => setCode(value)}
-            theme="vs-dark"
-          />
-
-          <div className="editor-label">Input</div>
-          <textarea name="input" value={input} onChange={(e) => setInput(e.target.value)} />
-          <br />
-          <button className="button" type="submit">Submit</button>
-        </form>
-      </div>
+        <div className="code-editor">
+          <h1>WriteYourCode.com</h1>
+          <form onSubmit={handleSubmit}>
+            <div className="editor-label">Language</div>
+            <select name="language" value={language} onChange={(e) => setLanguage(e.target.value)}>
+              <option value="cpp">C++</option>
+              <option value="python">Python</option>
+            </select> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <button className="button" type="submit">Submit</button>
+            <div className="editor-label">Code</div>
+            <Editor
+              className='Editor'
+              height="80vh"
+              width="120vh"
+              defaultPath="code.cpp"
+              language={language}
+              value={code}
+              onChange={(value) => setCode(value)}
+              theme="vs-dark"
+            />
+            <div className="editor-label">Input</div>
+            <textarea name="input" value={input} onChange={(e) => setInput(e.target.value)} />
+            <br />
+            <button className="button" type="submit">Submit</button>
+          </form>
+        </div>
       </div>
 
       <div className="output-container">
         <div className="status-container">
           <h2 className="subtitle">Status</h2>
-          <p className="status">{status}</p>
+          <p className="status" style={{color: statusColor}}>
+            {status}
+            {loadingImg && <img src={loadingImg} alt="loading" style={{ width: '20px', height: '20px', marginLeft: '10px' }} />}
+          </p>
           {error && <p className="error">Error: {error}</p>}
         </div>
 
-        {/* <div className="job-id-container">
-          <h2 className="subtitle">Job ID</h2>
-          <p className="job-id">{jobId && `JobId: ${jobId}`}</p>
-        </div> */}
-
         <div className="output">
           <h2 className="subtitle">Output</h2>
-          {status === "SUCCESS" && <pre>{output}</pre>}
+          {status === "SUCCESS" && <textarea className='output-area' readOnly value={output} />}
         </div>
       </div>
     </div>
